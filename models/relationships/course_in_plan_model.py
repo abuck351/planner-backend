8fd2a0ce002831b5f394c419a47cc4f28d039c94
Aaron Buckles <abuck351@gmail.com>
2020-03-22 22:16:41 -0700

    Changes CoursesInPlans schema

--- models/relationships/course_in_plan_model.py
+++ models/relationships/course_in_plan_model.py
@@ -6,11 +6,8 @@ from utils.db import db
 class CourseInPlanModel(db.Model):
     __tablename__ = "CoursesInPlans"
 
-    _id = db.Column(db.Integer, primary_key=True)
-    plan_id = db.Column(db.Integer, db.ForeignKey("Plans._id"))
-    course_id = db.Column(db.Integer, db.ForeignKey("Courses._id"))
-
-    __table_args__ = (db.UniqueConstraint("plan_id", "course_id"),)
+    plan_id = db.Column(db.Integer, db.ForeignKey("Plans._id"), primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey("Courses._id"), primary_key=True)
 
     def json(self) -> Dict:
         return {"plan_id": self.plan_id, "course_id": self.course_id}
