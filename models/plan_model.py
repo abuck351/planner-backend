ffeb4a638c9cd6e739b0e45bccefd7a4e90a5750
Aaron Buckles <abuck351@gmail.com>
2020-03-29 14:54:46 -0700

    Adds coures to plan resource

--- models/plan_model.py
+++ models/plan_model.py
@@ -1,6 +1,9 @@
+import traceback
 from typing import Dict
 
-from utils.db import db
+from utils.db import db, find_by
+from models.course_model import CourseModel
+from models.relationships.course_in_plan_model import CourseInPlanModel
 
 
 class PlanModel(db.Model):
@@ -13,4 +16,22 @@ class PlanModel(db.Model):
     __table_args__ = (db.UniqueConstraint("name", "term"),)
 
     def json(self) -> Dict:
-        return {"_id": self._id, "name": self.name, "term": self.term}
+        courses = []
+        try:
+            courses_in_plan = find_by(CourseInPlanModel, plan_id=self._id).all()
+        except Exception as e:
+            traceback.print_exc()
+        else:
+            for course_in_plan in courses_in_plan:
+                try:
+                    course = find_by(CourseModel, _id=course_in_plan.course_id).first()
+                    courses.append(course.json())
+                except Exception as e:
+                    traceback.print_exc()
+
+        return {
+            "_id": self._id,
+            "name": self.name,
+            "term": self.term,
+            "courses": courses,
+        }
